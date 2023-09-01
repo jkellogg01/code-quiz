333de20ff4d69a96df10e3601b292e45d82decc4
Joshua Kellogg <kellogg.20.joshua@gmail.com>
2023-09-01 09:29:21 -0600

    quiz currently doesn't end the way that it should

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,11 @@
 const topLineEl = $("#top-line");
-const quizSpaceEl = $("quiz-space");
-const quizStartBtn = $("quiz-start");
+const quizSpaceEl = $("#quiz-space");
+const quizStartBtn = $("#quiz-start");
+const quizTimeEl = $("#quiz-time-display");
+
+let score;
+let quizTime = 0;
+let listScores = JSON.parse(localStorage.getItem("listScores")) || [];
 
 const quizQuestions = [
   {
@@ -18,30 +23,69 @@ const quizQuestions = [
     answers: ["Jarring Story", "Juicy Socks", "JavaScript", "JumpStart"],
     correct: 2,
   },
+  {
+    body: "What does HTML stand for?",
+    answers: [
+      "Hypertext Markup Language",
+      "Huge Tortoise, Made Larger",
+      "Hard Times Might Last",
+      "Hurtling Towards Major Losses",
+    ],
+    correct: 0,
+  },
 ];
 
-quizStartBtn.on("click", (event) => {
-  quizStartBtn.css("display: none");
-  let quizTime = 75;
+console.log(quizStartBtn.text());
+
+quizStartBtn.on("click", handleQuiz);
+
+quizSpaceEl.on("click", handleAnswer);
+
+function handleQuiz(event) {
+  console.log("something");
+  score = 0;
+  quizTime = 75;
+  generateQuestion();
   setInterval(function quizTimer() {
     if (quizTime <= 0) {
-      quizStartBtn.css("display: inline-block");
+      scorePrompt(score);
       clearInterval(quizTimer);
+      quizSpaceEl.children().remove();
+      return;
     }
+    quizTimeEl.text("Time: " + quizTime);
+    quizTime--;
   }, 1000);
-  while (quizTime > 0) {
-    generateQuestion();
-  }
-});
+}
 
 function generateQuestion() {
+  quizSpaceEl.children().remove();
   const question =
     quizQuestions[Math.floor(Math.random() * quizQuestions.length)];
   topLineEl.text(question.body);
-  question.answers.forEach((value, index, array) => {
-    let answerButton = quizSpaceEl.add("<button>");
+  question.answers.forEach((value, index) => {
+    let answerButton = $("<button>");
     let innerText = String(index + 1) + ". " + value;
     answerButton.text(innerText);
-    answerButton.on("click", () => {});
+    quizSpaceEl.append(answerButton);
   });
+  quizSpaceEl.children().eq(question.correct).addClass("correct");
+}
+
+function handleAnswer(event) {
+  let element = $(event.target);
+  console.log(element);
+  if (!element.hasClass("correct")) {
+    quizTime -= 10;
+    return;
+  }
+  score++;
+  console.log(score);
+  if (quizTime > 0) {
+    generateQuestion();
+  }
+}
+
+function scorePrompt() {
+  console.log("this doesn't do anything yet.");
 }
